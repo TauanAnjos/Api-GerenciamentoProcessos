38c2ad6ae570cae650d2e5de064a2ad2d06b41dc
tauandosanjosnascimento <tauandosanjosnascimento@gmail.com>
2025-03-19 14:51:10 -0300

    feath(Controller: PrazoController) implementado CRUD.

--- GerenciamentoProcessos/Controllers/PrazoController.cs
+++ GerenciamentoProcessos/Controllers/PrazoController.cs
@@ -1,4 +1,5 @@
-﻿using GerenciamentoProcessos.Services.Interfaces;
+﻿using GerenciamentoProcessos.Controllers.Dtos;
+using GerenciamentoProcessos.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GerenciamentoProcessos.Controllers;
@@ -11,5 +12,94 @@ public class PrazoController : ControllerBase
     public PrazoController(IPrazoAppService prazoAppService)
     {
         _prazoAppService = prazoAppService;
+    }
+
+    [HttpPost]
+    public IActionResult CriarPrazo(CriarPrazoDto criarPrazoDto)
+    {
+        if (criarPrazoDto == null)
+        {
+            return BadRequest("Os dados de prazo são obrigatorios.");
+        }
+        try
+        {
+            _prazoAppService.CriarPrazo(criarPrazoDto);
+            return Created("api/v1/prazo", criarPrazoDto);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpGet("{id}")]
+    public IActionResult BuscarPrazoPorId([FromRoute] Guid id)
+    {
+        var prazoExistente = _prazoAppService.BuscarPrazoPorId(id);
+
+        if (prazoExistente == null)
+        {
+            return NotFound($"Prazo de ID {id} não encontrado.");
+        }
+        try
+        {
+            return Ok(prazoExistente);
+        }
+        catch (Exception ex) 
+        {
+            return BadRequest(ex.Message);
+        }
+
+    }
+    [HttpGet]
+    public IActionResult ListarPrazos()
+    {
+        try
+        {
+            var prazos = _prazoAppService.ListarPrazos();
+            return Ok(prazos);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpPut("{id}")]
+    public IActionResult EditarPrazo([FromRoute]Guid id,[FromBody] EditarPrazoDto editarPrazoDto)
+    {
+        var prazo = _prazoAppService.BuscarPrazoPorId(id);
+
+        if(prazo == null)
+        {
+            return NotFound($"Prazo de ID{id} não encontrado.");
+        }
+
+        try
+        {
+            _prazoAppService.EditarPrazo(id, editarPrazoDto);
+            var prazoAtualizado = _prazoAppService.BuscarPrazoPorId(id);
+            return Ok(prazoAtualizado);
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
+    }
+    [HttpDelete("{id}")]
+    public IActionResult DeletePrazo([FromRoute] Guid id)
+    {
+        var prazoExistente = _prazoAppService.BuscarPrazoPorId(id);
+
+        if (prazoExistente == null)
+        {
+            return NotFound($"Prazo de ID {id} não encontrado.");
+        }
+        try
+        {
+            _prazoAppService.DeletarPrazo(id);
+            return NoContent();
+        }catch(Exception ex)
+        {
+            return BadRequest(ex.Message);
+        }
     }
 }
